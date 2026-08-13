8364707df096027eccb36591cf0c61abc60cca86
Devanshi Singh <devanshisingh.1410@gmail.com>
2026-08-14 02:17:59 +0530

    Time: 8 ms (70.5%), Space: 47.7 MB (62.76%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -0,0 +1,15 @@
+class NumArray {
+    int[] arr;
+
+    public NumArray(int[] nums) {
+        arr = new int[nums.length + 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            arr[i + 1] = arr[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return arr[right + 1] - arr[left];
+    }
+}
